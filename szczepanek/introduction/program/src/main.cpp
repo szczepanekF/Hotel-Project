cd223499596c1619c0388986abba5f67127c2019
242544 Szczepanek Filip <242544@edu.p.lodz.pl>
2022-03-12 02:00:19 +0100

    Naprawa 3

---
+++ szczepanek/introduction/program/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "math_helpers.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << factorial(5)   << endl;
+    return 0;
+}
+
